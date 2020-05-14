9a0100c8439890409aa2706a26bfe5c122566ed8
Rodrigo Souza <rodrigo.augusto93@gmail.com>
2020-05-14 10:29:15 -0300

    Project to count people

---
+++ README.md
@@ -0,0 +1,5 @@
+# contador_de_pessoas
+
+Um projeto desenvolvido em flutter para contar a quantidade de pessoas.
+
+
---
+++ android/app/src/main/kotlin/br/com/rsouza/contador_de_pessoas/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.rsouza.contador_de_pessoas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
